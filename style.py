44540465c4c3e5df1fe867f44287ab6b52a869d7
Adrian Kuca <adriankuca2@gmail.com>
2021-09-28 21:17:12 +0200

    Add file for consistent color styling

---
+++ style.py
@@ -0,0 +1,23 @@
+FIG_BG_COLOR = "#0B1629"
+AX_BG_COLOR = "#0D1A31"
+AX_LABEL_COLOR = "#41646C"
+TICKS_COLOR = "#41646C"
+
+DEFAULT_LINE_COLOR = "#19444D"
+
+RED = "#660000"
+GREEN = "#023b08"
+BLUE = "#164ACD"
+
+
+def set_plot_style(fig, axes):
+    fig.patch.set_facecolor(FIG_BG_COLOR)
+    for ax in axes:
+        ax.patch.set_facecolor(AX_BG_COLOR)
+        ax.spines["top"].set_color(TICKS_COLOR)
+        ax.spines["bottom"].set_color(TICKS_COLOR)
+        ax.spines["left"].set_color(TICKS_COLOR)
+        ax.spines["right"].set_color(TICKS_COLOR)
+        ax.tick_params(colors=TICKS_COLOR)
+        ax.xaxis.label.set_color(AX_LABEL_COLOR)
+        ax.yaxis.label.set_color(AX_LABEL_COLOR)
